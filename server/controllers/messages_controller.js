0fb536503620f09cbc3e16cf1121f804fff104dd
Emmanuel Garzon <eggarzon@gmail.com>
2022-06-10 12:23:59 -0700

    all endpoints created

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -2,8 +2,36 @@ let messages = [];
 let id = 0;
 
 module.exports = {
-  create: (req, res) => {},
-  read: (req, res) => {},
-  update: (req, res) => {},
-  delete: (req, res) => {},
+  msgCreate: (req, res) => {
+    const { text, time } = req.body;
+    messages.push({ id, text, time });
+    id++;
+    res.status(200).send(messages);
+  },
+
+  msgRead: (req, res) => {
+    res.status(200).send(messages);
+  },
+
+  msgUpdate: (req, res) => {
+    const { text } = req.body;
+    const updateID = req.params.id;
+    const messageIndex = messages.findIndex(message.id == updateID);
+    let message = messages[messageIndex];
+
+    messages[messageIndex] = {
+      id: message.id,
+      text: text || message.text,
+      time: message.time,
+    };
+
+    res.status(200).send(messages);
+  },
+
+  msgDelete: (req, res) => {
+    const deleteID = req.params.id;
+    const messageIndex = messages.findIndex(message.id === deleteID);
+    messages.splice(messageIndex, 1);
+    res.status(200).send(messages);
+  },
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+
+const {
+  msgCreate,
+  msgRead,
+  msgUpdate,
+  msgDelete,
+} = require("./controllers/messages_controller.js");
+
 app.use(express.json());
+app.use(express.static(__dirname + "/../public/build"));
+
+const baseURL = "/api/messages";
+app.post(baseURL, msgCreate);
+app.get(baseURL, msgRead);
+app.put(`${baseURL}/:id`, msgUpdate);
+app.delete(`${baseURL}/:id`, msgDelete);
 
 const port = 3001;
 app.listen(port, () => {
